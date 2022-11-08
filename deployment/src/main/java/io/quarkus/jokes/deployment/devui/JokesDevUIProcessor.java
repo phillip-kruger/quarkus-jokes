83f3b427bb51fd1de113a686d7e054f952771208
Phillip Kruger <phillip.kruger@gmail.com>
2022-11-08 14:47:09 +1100

    Added qute build time example
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- deployment/src/main/java/io/quarkus/jokes/deployment/devui/JokesDevUIProcessor.java
+++ deployment/src/main/java/io/quarkus/jokes/deployment/devui/JokesDevUIProcessor.java
@@ -1,5 +1,7 @@
 package io.quarkus.jokes.deployment.devui;
 
+import java.util.Map;
+
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.devui.deployment.spi.page.Page;
 import io.quarkus.devui.deployment.spi.page.PageBuildItem;
@@ -8,7 +10,7 @@ import io.quarkus.jokes.deployment.JokesBuildItem;
 /**
  * This example show how to show some pages in Dev UI
  */
-public class DevUIJokesProcessor {
+public class JokesDevUIProcessor {
 
     @BuildStep
     PageBuildItem createJokes(JokesBuildItem jokesBuildItem) {
@@ -23,39 +25,42 @@ public class DevUIJokesProcessor {
                 .icon("font-awesome-solid:arrow-up-right-from-square")
                 .label("external")
                 .url("https://randomuser.me/")
-                .isHtmlContent()
-                .build());
+                .isHtmlContent());
 
         pageBuildItem.addPage(Page.externalPageBuilder("External Json")
                 .icon("font-awesome-solid:arrow-up-right-from-square")
                 .label("external")
                 .url("https://official-joke-api.appspot.com/jokes/ten")
-                .isJsonContent()
-                .build());
+                .isJsonContent());
 
         // 2) Page that show build time data in raw json format
         pageBuildItem.addPage(Page.rawDataPageBuilder("Raw data")
                 .icon("font-awesome-brands:js")
-                .label("data")
-                .buildTimeDataKey("jokes")
-                .build());
+                .label("build-time")
+                .buildTimeDataKey("jokes"));
 
         // 3) Page that show build time data in a table
         pageBuildItem.addPage(Page.tableDataPageBuilder("Table data")
                 .icon("font-awesome-solid:table")
-                .label("data")
+                .label("build-time")
                 .showColumn("timestamp")
                 .showColumn("user")
                 .showColumn("fullJoke")
-                .buildTimeDataKey("jokes")
-                .build());
+                .buildTimeDataKey("jokes"));
+
+        // 4) Page that show build time data that has been formatted using Qute
+
+        pageBuildItem.addPage(Page.quteDataPageBuilder("Qute data")
+                .icon("font-awesome-solid:q")
+                .label("build-time")
+                .data(Map.of("jokes", jokesBuildItem.getJokes()))
+                .templateLink("qute-jokes-template.html"));
 
         // Page create with custom web component
         pageBuildItem.addPage(Page.webComponentPageBuilder()
                 .icon("font-awesome-solid:cubes")
                 .componentLink("qwc-jokes-web-components.js")
-                .label("build-time")
-                .build());
+                .label("build-time"));
 
         return pageBuildItem;
     }
